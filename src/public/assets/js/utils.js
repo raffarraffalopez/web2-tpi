1026d2365ef1cfd6b7ca69b223fef85ebf7bcbf7
raffarraffalopez <92556734+raffarraffalopez@users.noreply.github.com>
2023-09-22 22:39:10 -0300

    Delete src directory

--- src/public/assets/js/utils.js
+++
@@ -1,8 +0,0 @@
-function debug(msg) {
-    const stack = new Error().stack.split('\n');
-    let partes = stack[2].split('\/');
-    partes = stack[2].split('\/')[stack[2].split('\/').length - 1].split(':');
-    console.log('\n\r', partes[0], 'linea', partes[1], ' -> ', msg);
-    //console.log('\n\r', partes[1], ' -> ', msg);
-}
-export default debug;
